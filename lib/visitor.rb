ed205ae9cd3c7c70b9023f6d9b59a2267551160a
Rithreaksa <wonderfulreaksa@gmail.com>
2020-10-31 21:22:47 +0100

    Writing more tests and code for borrow method. Running ang passing the test

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -8,21 +8,27 @@ class Visitor
         @books = []
     end
 
-    def borrow arg
-        missing_library
+    def borrow arg # has to be {list: ..., library: ...}
+        missing_library arg[:library]
+        missing_books arg[:list]
+        library = arg[:library]
+        list = arg[:list]
+        result = library.borrow(list)
+        if result[:status] == true then
+            @books += result[:books]
+        end
+        result[:message]
     end
 
-    def missing_library
-        raise 'Library is required'
+    def missing_library library
+        raise 'Library is required' if library == nil
     end
 
     def list_of_books
-        true
+        books
     end
 
-
-
-  #  def missing_books
-   #     raise 'List of books is required'
-   # end
+   def missing_books list
+       raise 'List of books is required' if list == nil
+   end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,12 +3,12 @@ require 'date'
 
 describe Visitor do
     let(:name) {'Sanne'}
-    let(:library) {instance_double ('Library', books: [])}
+    let(:library) { instance_double('Library', books: []) }
     subject {Visitor.new(name)}
 
     before do 
         allow(library).to receive(:borrow)
-        allow(library).to receive(:books).and_return([])
+        # allow(library).to receive(:books).and_return([])
     end
 
     it 'is expected to have a name on initialize' do
@@ -23,17 +23,89 @@ describe Visitor do
         expect{ subject.borrow({}) }.to raise_error ('Library is required')
     end
 
-   it 'is expected to raise an error if there is no list of books' do
+    it 'is expected to raise an error if there is no list of books' do
        expect{ subject.borrow({library: library}) }.to raise_error ('List of books is required')
     end
 
-   it 'is expected to have a list of books' do
-    # he would see a list of books with titles and authors and maybe avalibility(true/false)
-    expect(subject.list_of_books).to be_an(String)
+    it 'is expected to have a list of books' do
+        expect(subject.list_of_books).to be_an(Array)
     end
 
-    it 'is expected to update the booklist' do
+    it 'is expected to update the books when borrow success' do
         # he would see the list of books that he borrows with the titles, authors and return date
+        book_to_be_borrowed = {
+            title: "Apple",
+            author: "A",
+            return_date: Date.today
+        }
+        title_to_be_borrowed = book_to_be_borrowed[:title]
+
+        subject.books = []
+
+        allow(library).to receive(:borrow).and_return({
+            status: true,
+            message: "success",
+            books: [book_to_be_borrowed]
+        })
+
+        expect(library).to receive(:borrow).with([title_to_be_borrowed])
+        
+        subject.borrow({
+            list: [title_to_be_borrowed],
+            library: library
+        })
+
+        expect(subject.books).to eq [book_to_be_borrowed]
     end
 
+    it 'is expected to updates the books when books not available' do
+        title_to_be_borrowed = "Apple"
+        subject.books = []
+        allow(library).to receive(:borrow).and_return({
+            status: false,
+            message: "not available",
+        })
+
+        expect(library).to receive(:borrow).with([title_to_be_borrowed])
+        subject.borrow({
+            list: [title_to_be_borrowed],
+            library: library
+        })
+        expect(subject.books).to eq []
+    end
 end
+
+=begin
+
+data --> library.list_of_books --> visitor
+data <-- library.borrow <-- visitor.borrow
+data <-- library.return <-- visotor.return
+
+visitor = Visitor.new
+visitor
+=> {books:[]}
+library1 = Library.new "./lib/data1.yaml"
+library2 = Library.new "./lib/data2.yaml"
+library.list_of_books
+=> [{title: "Apple", author: "A", status: :available}, {title: "Banana", author: "B", status: :not_available}]
+visitor.borrow(list: ["Apple"], library: library1)
+...
+-- library should take ["Apple"] # result = library.process(["Apple"])
+-- result is {
+    status: true,
+    message: "success",
+    books: [{title: "Apple", author: "A", status: :available}]
+   }
+-- result is {status: false, message: "not exist / not available"}
+status = result[:status]
+if status then
+    @books = @books + result[:books] # [{A}] + [{B}] => [{A}, {B}]
+do
+
+visitor
+=> {books: [{title: "Apple", author: "A", status: :available}]}
+expect(subject.books).to eq {books: [{title: "Apple", author: "A", status: :available}]}
+library
+=> {..., list_of_books:[{title: "Apple", author: "A", status: :not_available}, {title: "Banana", author: "B", status: :not_available}]}
+
+=end
